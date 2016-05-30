483709e329d15fe09751aae4ba57660c521466db
Colo <apredazzi@gmail.com>
2016-05-30 15:40:14 -0300

    Agrego todas las diferentes corridas en una carpeta nueva, con sus dat, txt y sol, y ademas cambio la heuristica de python para que contemple lo del presupuesto

--- GranDT.py
+++ GranDT.py
@@ -5,82 +5,82 @@ import operator
 import sys
 import functools
 
+def generarEquipo(datos, defensas, volantes, delanteros):
+	jugadores = []
+	jugadores += datos["ARQ"][-1:]
+	datos["ARQ"] = datos["ARQ"][:-1]
+	jugadores += datos["DEF"][-defensas:]
+	datos["DEF"] = datos["DEF"][:-defensas]
+	jugadores += datos["VOL"][-volantes:]
+	datos["VOL"] = datos["VOL"][:-volantes]
+	jugadores += datos["DEL"][-delanteros:]
+	datos["DEL"] = datos["DEL"][:-delanteros]
 
-with open('/home/colopreda/ClionProjects/GranDT/GranDT2015_formatted.csv', newline='') as csvfile:
+	jugadores.sort(key=lambda x: int(x[3]))
+	return jugadores
+
+def reemplazarJugadorMasCaro(datos, jugadores):
+	mascaro = jugadores.pop()
+	reemplazo = datos[mascaro[1]][-1:]
+	datos[mascaro[1]] = datos[mascaro[1]][:-1]
+
+	jugadores += reemplazo
+	jugadores.sort(key=lambda x: int(x[3]))
+
+def sortPorPos(x):
+	l = ["ARQ", "DEF", "VOL", "DEL"]
+	return l.index(x[1])
+
+with open('GranDT2015_formatted_python.csv', newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 	jugadores = sorted(reader, key=operator.itemgetter(23), reverse=True)
 	jugadores.sort(key=lambda x: int(x[23]))
 
-	arquero = [k for k in jugadores if 'ARQ' in k]
-	defensa = [k for k in jugadores if 'DEF' in k]
-	volante = [k for k in jugadores if 'VOL' in k]
-	delantero = [k for k in jugadores if 'DEL' in k]
-
-	trescuatrotres = arquero[-1:]
-	trescuatrotres.extend(defensa[-3:])
-	trescuatrotres.extend(volante[-4:])
-	trescuatrotres.extend(delantero[-3:])
-
+	datos = {
+	"ARQ" : [k for k in jugadores if 'ARQ' in k],
+	"DEF" : [k for k in jugadores if 'DEF' in k],
+	"VOL" : [k for k in jugadores if 'VOL' in k],
+	"DEL" : [k for k in jugadores if 'DEL' in k],
+	}
+	datos343 = datos.copy()
+	trescuatrotres = generarEquipo(datos343, 3, 4, 3)
 	suma343 = sum(int(row[23]) for row in trescuatrotres)
 
-	cuatrocuatrodos = arquero[-1:]
-	cuatrocuatrodos.extend(defensa[-4:])
-	cuatrocuatrodos.extend(volante[-4:])
-	cuatrocuatrodos.extend(delantero[-2:])
-
+	datos442 = datos.copy()
+	cuatrocuatrodos = generarEquipo(datos442, 4, 4, 2)
 	suma442 = sum(int(row[23]) for row in cuatrocuatrodos)
 
-	cuatrotrestres = arquero[-1:]
-	cuatrotrestres.extend(defensa[-4:])
-	cuatrotrestres.extend(volante[-3:])
-	cuatrotrestres.extend(delantero[-3:])
-
+	datos433 = datos.copy()
+	cuatrotrestres = generarEquipo(datos433, 4, 3, 3)
 	suma433 = sum(int(row[23]) for row in cuatrotrestres)
 
 	if (suma343 >= suma433 and suma343 >= suma442):
 		formacionSel = trescuatrotres
+		datosSel = datos343
 		print("La formacion seleccionada es 1-3-4-3 con los siguientes jugadores:\n")
 	elif (suma433 >= suma343 and suma433 >= suma442):
 		formacionSel = cuatrotrestres
+		datosSel = datos433
 		print("La formacion seleccionada es 1-4-3-3 con los siguientes jugadores:\n")
 	else:
 		formacionSel = cuatrocuatrodos
+		datosSel = datos442
 		print("La formacion seleccionada es 1-4-4-2 con los siguientes jugadores:\n")
 
+	cotizacion = sum(int(row[3]) for row in formacionSel)
+	while (cotizacion > 65000000):
+		reemplazarJugadorMasCaro(datosSel, formacionSel)
+		cotizacion = sum(int(row[3]) for row in formacionSel)
+
+	formacionSel = sorted(formacionSel, key = sortPorPos)
+
+	print ("{:<20}{:^10}{:<20}{:<15}{:<10}".format("Nombre","Pos","Equipo", "Cotizacion", "Puntaje"))
+	print ("{:<20}{:^10}{:<20}{:<15}{:<10}\n".format("-"*6,"-"*3,"-"*6, "-"*10, "-"*7))
 	for row in formacionSel:				
-		print(' - '.join(list( row[i] for i in [0, 1, 2, 3, 23] )))
+		print ("{:<20}{:^10}{:<20}{:<15}{:<10}".format(row[0],row[1],row[2], row[3], row[23]))
 
 	puntaje = sum((sum([int(player[i]) for player in formacionSel])) for i in range(4, 19))
 	puntaje += sum((max([int(player[i]) for player in formacionSel])) for i in range(4, 19))
 	print("\nEl puntaje obtenido es: ", puntaje)
-	cotizacion = sum(int(row[3]) for row in formacionSel)
-	print("La cotización total del equipo es: ", cotizacion)
 
-
-	# 	print("La formacion seleccionada es 1-3-4-3 con los siguientes jugadores:\n")
-	# 	for row in trescuatrotres:				
-	# 		print(' - '.join(list( row[i] for i in [0, 1, 2, 3, 20] )))
-	# 	suma343 += sum((max([int(player[i]) for player in trescuatrotres])) for i in range(4, 19))
-	# 	print("\nEl puntaje obtenido es: ", suma343)
-
-	# if (suma433 >= suma343 and suma433 >= suma442):
-	# 	print("La formacion seleccionada es 1-4-4-2 con los siguientes jugadores:\n")
-	# 	for row in cuatrotrestres:				
-	# 		print(' - '.join(list( row[i] for i in [0, 1, 2, 3, 20] )))
-	# 	suma433 += sum((max([int(player[i]) for player in trescuatrotres])) for i in range(4, 19))
-	# 	print("\nEl puntaje obtenido es: ", suma433)
-
-	# if (suma442 >= suma343 and suma442 >= suma433):
-	# 	print("La formacion seleccionada es 1-4-3-3 con los siguientes jugadores:\n")
-	# 	for row in cuatrocuatrodos:				
-	# 		print(' - '.join(list( row[i] for i in [0, 1, 2, 3, 20] )))
-
-	# 	print("\nEl puntaje obtenido es: ", suma442)
-
-
-	# print("--------------------------------------------------------------")
-
-	# print("<><><><><><><><><><><><>")
-	# [print(str(i-3)+":"+str(max([int(player[i]) for player in trescuatrotres]))) for i in range(4, 19)]
-
-	# Puntaje con GLPK = 1676
+	print("La cotizacion total del equipo es: ", cotizacion)
---
+++ correr.sh
@@ -0,0 +1,1 @@
+glpsol -m grandt.mod -d grandt4.dat -o sol4sc.o --tmlim 10800 --log salida4sc.txt
