2900f541a362b0b383efb5293c468748d92a7b09
Colo <apredazzi@gmail.com>
2016-06-04 20:16:27 -0300

    Commit inicial de la parte 2, donde para cada fecha se elige el mejor equipo, sin tener en cuenta el anterior

---
+++ GranDT-Parte2.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+
+import csv
+import operator
+import sys
+import functools
+from collections import Counter
+
+JUG_POR_EQUIPO = 3
+COTIZACION_MAX = 65000000
+FECHAS = 15
+COLUMNA_FECHA_1 = 4
+
+def equipoMas3Players(jugadores):
+	#equipos = []
+	#for row in jugadores:
+	#	equipos.append(row[2]) 
+
+	equipos = [j[2] for j in jugadores]
+
+	cnt = Counter(equipos)
+	equiposMas3 = [k for k, v in cnt.items() if v > JUG_POR_EQUIPO]
+	return equiposMas3
+
+def cambiarJugador(equiposMas3, jugadores, datos):
+	#jugadoresAfectados = [] 
+	#for s in jugadores:
+	#	for item in equiposMas3:
+	#		if item in s:
+	#			jugadoresAfectados.append(s)
+
+	jugadoresAfectados = [j for j in jugadores if j[2] in equiposMas3]
+
+	if jugadoresAfectados:
+		jugadorASacarEnPos = jugadores.index(jugadoresAfectados[0])
+		jugadorSuperado = jugadores.pop(jugadorASacarEnPos)
+		reemplazo = datos[jugadorSuperado[1]][-1:]
+		datos[jugadorSuperado[1]] = datos[jugadorSuperado[1]][:-1]
+
+		jugadores += reemplazo
+		jugadores.sort(key=lambda x: int(x[3]))
+
+def generarEquipo(datos, defensas, volantes, delanteros):
+	jugadores = []
+	jugadores += datos["ARQ"][-1:]
+	datos["ARQ"] = datos["ARQ"][:-1]
+	jugadores += datos["DEF"][-defensas:]
+	datos["DEF"] = datos["DEF"][:-defensas]
+	jugadores += datos["VOL"][-volantes:]
+	datos["VOL"] = datos["VOL"][:-volantes]
+	jugadores += datos["DEL"][-delanteros:]
+	datos["DEL"] = datos["DEL"][:-delanteros]
+
+	jugadores.sort(key=lambda x: int(x[3]))
+	return jugadores
+
+def reemplazarJugadorMasCaro(datos, jugadores):
+	mascaro = jugadores.pop()
+	reemplazo = datos[mascaro[1]][-1:]
+	datos[mascaro[1]] = datos[mascaro[1]][:-1]
+
+	jugadores += reemplazo
+	jugadores.sort(key=lambda x: int(x[3]))
+
+def sortPorPos(x):
+	l = ["ARQ", "DEF", "VOL", "DEL"]
+	return l.index(x[1])
+
+for x in range(COLUMNA_FECHA_1, COLUMNA_FECHA_1+FECHAS):
+	with open('GranDT2015_formatted_python.csv', newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		jugadores = sorted(reader, key=operator.itemgetter(x), reverse=True)
+		jugadores.sort(key=lambda y: int(y[x]))
+
+		print("X VALE: %d" % x)
+		datos = {
+		"ARQ" : [k for k in jugadores if 'ARQ' in k],
+		"DEF" : [k for k in jugadores if 'DEF' in k],
+		"VOL" : [k for k in jugadores if 'VOL' in k],
+		"DEL" : [k for k in jugadores if 'DEL' in k],
+		}
+		datos343 = datos.copy()
+		trescuatrotres = generarEquipo(datos343, 3, 4, 3)
+		suma343 = sum(int(row[x]) for row in trescuatrotres)
+
+		datos442 = datos.copy()
+		cuatrocuatrodos = generarEquipo(datos442, 4, 4, 2)
+		suma442 = sum(int(row[x]) for row in cuatrocuatrodos)
+
+		datos433 = datos.copy()
+		cuatrotrestres = generarEquipo(datos433, 4, 3, 3)
+		suma433 = sum(int(row[x]) for row in cuatrotrestres)
+
+		if (suma343 >= suma433 and suma343 >= suma442):
+			formacionSel = trescuatrotres
+			datosSel = datos343
+			print("La formacion seleccionada es 1-3-4-3 con los siguientes jugadores:\n")
+		elif (suma433 >= suma343 and suma433 >= suma442):
+			formacionSel = cuatrotrestres
+			datosSel = datos433
+			print("La formacion seleccionada es 1-4-3-3 con los siguientes jugadores:\n")
+		else:
+			formacionSel = cuatrocuatrodos
+			datosSel = datos442
+			print("La formacion seleccionada es 1-4-4-2 con los siguientes jugadores:\n")
+
+		cotizacion = sum(int(row[3]) for row in formacionSel)
+		while (cotizacion > COTIZACION_MAX):
+			reemplazarJugadorMasCaro(datosSel, formacionSel)
+			cotizacion = sum(int(row[3]) for row in formacionSel)
+
+		equiposMas3 = equipoMas3Players(formacionSel)
+		while (equiposMas3):
+			cambiarJugador(equiposMas3, formacionSel, datos)
+			equiposMas3 = equipoMas3Players(formacionSel)
+
+		formacionSel = sorted(formacionSel, key = sortPorPos)
+
+		print ("{:<25}{:^10}{:<20}{:<15}{:<10}".format("Nombre","Pos","Equipo", "Cotizacion", "Puntaje"))
+		print ("{:<25}{:^10}{:<20}{:<15}{:<10}\n".format("-"*6,"-"*3,"-"*6, "-"*10, "-"*7))
+		for row in formacionSel:				
+			print ("{:<25}{:^10}{:<20}{:<15}{:<10}".format(row[0],row[1],row[2], row[3], row[x]))
+		puntaje = sum(int(row[x]) for row in formacionSel)
+		#puntaje = sum((sum([int(player[i]) for player in formacionSel])) for i in range(4, 19))
+		#puntaje += sum((max([int(player[i]) for player in formacionSel])) for i in range(4, 19))
+		print("\nEl puntaje obtenido es: ", puntaje)
+
+		print("La cotizacion total del equipo es: ", cotizacion)
